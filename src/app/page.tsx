88cc906d4405740e5a3ccd80234ee338976ff5b5
matsu <matsuaki171101@gmail.com>
2023-09-26 15:07:24 +0900

    曲情報部分のレイアウト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ const page = () => {
     const hairetsu = [...Array(100)].map((v, i) => i)
     return (
         <div className=" bg-red-100 h-screen w-screen pt-20 px-8 pb-6 ">
-            <div className=" bg-blue-100 h-full flex flex-col overflow-y-scroll break-all ">
+            <div className=" bg-blue-100 h-full flex flex-col overflow-y-scroll text-center break-all py-2 px-6 ">
                 {hairetsu.map((i) => (
-                    <div>
+                    <div className=" bg-red-200 grow my-2 py-2 rounded-md ">
                         {i}
                     </div>
                 ))}
