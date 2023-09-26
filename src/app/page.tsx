75461d5314007563de5ad101371fc0ee8e6c956a
matsu <matsuaki171101@gmail.com>
2023-09-26 15:14:48 +0900

    微調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
         <div className=" bg-red-100 h-screen w-screen pt-20 px-8 pb-6 ">
             <div className=" bg-blue-100 h-full flex flex-col overflow-y-scroll text-center break-all py-2 px-6 ">
                 {hairetsu.map((i) => (
-                    <div className=" bg-red-200 grow my-2 py-2 rounded-md ">
+                    <div className=" bg-red-200 grow my-2 py-6 rounded-md ">
                         {i}
                     </div>
                 ))}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,17 +5,19 @@ const SideBar = ({ anchorEl, handleInVisible }: { anchorEl: Visibility, handleIn
     const hairetsu = [...Array(100)].map((v, i) => i)
     return (
         <div className={" fixed overflow-y-scroll top-0 left-0 h-screen w-5/12 z-10 py-4 pr-4 pl-2 bg-yellow-100 " + anchorEl}  >
-            <div className=" max-h-fit w-full flex ">
+            <div className=" max-h-fit w-full flex mb-2 ">
                 <div className=" grow"></div>
                 <div className=" bg-blue-100 py-1 px-2 rounded-md " onClick={handleInVisible} aria-label="閉じるボタン">
                     閉じる
                 </div>
             </div>
-            {hairetsu.map((i) => (
-                <div>
-                    {i}
-                </div>
-            ))}
+            <div className=" ml-1 ">
+                {hairetsu.map((i) => (
+                    <div>
+                        {i}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
