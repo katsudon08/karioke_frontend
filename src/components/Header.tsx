29d0a99ae9062a37e60597d25a25be18f3898726
matsu <matsuaki171101@gmail.com>
2023-09-26 14:58:40 +0900

    ヘッダー、サイドバー、メインコンテンツのレイアウト完成

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Visibility } from "@/types";
+
+const Header = ({ handleVisible }: { handleVisible: () => void }) => {
+    return (
+        <header className=" min-w-full h-14 top-0 left-0 py-2 px-6 fixed z-0 bg-white ">
+            <div className=" min-w-full min-h-full flex justify-center items-center ">
+                <div className=" min-w-full flex justify-between ">
+                    <div aria-label="メニューボタン" className=" bg-blue-100 py-1 px-2 rounded-md " onClick={handleVisible}>
+                        メニュー
+                    </div>
+                    <div aria-label="アカウント画面遷移ボタン" className=" bg-blue-100 py-1 px-2 rounded-md ">
+                        アカウント
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Home.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useEffect, useState } from 'react'
+import { Visibility } from '@/types'
+import Header from '@/components/Header'
+import SideBar from '@/components/SideBar'
+
+const Home = () => {
+    const [anchorEl, setAnchorEl] = useState<Visibility>("invisible")
+
+    const handleVisible = () => {
+        setAnchorEl("visible")
+    }
+
+    const handleInVisible = () => {
+        setAnchorEl("invisible")
+    }
+
+    return (
+        <div className=' h-full w-full '>
+            <SideBar anchorEl={anchorEl} handleInVisible={handleInVisible} />
+            <Header handleVisible={handleVisible} />
+        </div>
+    );
+}
+
+export default Home;
