de2f0f26edd611810ec44a5333b5b0199d524504
matsu <matsuaki171101@gmail.com>
2023-09-27 12:47:30 +0900

    レビュー部分が見た目だけ完成

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,7 +3,7 @@
 import { FormEvent, useState } from "react";
 
 const page = () => {
-    const [colorFlags, setColorFlag] = useState([false, false, false, false, false])
+    const [colors, setColor] = useState(["text-gray-300", "text-gray-300", "text-gray-300", "text-gray-300", "text-gray-300"])
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
@@ -12,12 +12,10 @@ const page = () => {
     }
 
     const handleColorChange = (num: number) => {
-        setColorFlag([...Array(5)].map((v: boolean, i: number) => (
-            v = i > num ? false : true
+        setColor([...Array(5)].map((v: string, i: number) => (
+            v = i <= num ? "text-yellow-400" : "text-gray-300"
         )))
     }
-
-    const colors = colorFlags ? "text-yellow-400" : "text-gray-400"
 
     return (
         <div className=" bg-blue-100 h-full flex flex-col items-center text-center break-all py-6 px-6 ">
@@ -34,22 +32,25 @@ const page = () => {
                         name="singer"
                     />
                     <div className="">
-                        <p>レビュー</p>
+                        <p>頻度</p>
                         <div>
-                            <span className=" flex flex-row-reverse justify-end ">
-                                <input id="0" type="radio" className=" hidden " onChange={() => handleColorChange(0)}/><label htmlFor="0" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[0]}>★</label>
-                                <input id="1" type="radio" className=" hidden " onChange={() => handleColorChange(1)}/><label htmlFor="1" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[1]}>★</label>
-                                <input id="2" type="radio" className=" hidden " onChange={() => handleColorChange(2)}/><label htmlFor="2" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[2]}>★</label>
-                                <input id="3" type="radio" className=" hidden " onChange={() => handleColorChange(3)}/><label htmlFor="3" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[3]}>★</label>
-                                <input id="4" type="radio" className=" hidden " onChange={() => handleColorChange(4)}/><label htmlFor="4" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[4]}>★</label>
+                            <span className=" flex justify-center ">
+                                <input id="review01" type="checkbox" className=" hidden " onClick={() => handleColorChange(0)} /><label htmlFor="review01" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[0]}>★</label>
+                                <input id="review02" type="checkbox" className=" hidden " onClick={() => handleColorChange(1)} /><label htmlFor="review02" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[1]}>★</label>
+                                <input id="review03" type="checkbox" className=" hidden " onClick={() => handleColorChange(2)} /><label htmlFor="review03" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[2]}>★</label>
+                                <input id="review04" type="checkbox" className=" hidden " onClick={() => handleColorChange(3)} /><label htmlFor="review04" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[3]}>★</label>
+                                <input id="review05" type="checkbox" className=" hidden " onClick={() => handleColorChange(4)} /><label htmlFor="review05" className={" text-3xl px-2 cursor-pointer hover:text-yellow-400 " + colors[4]}>★</label>
                             </span>
                         </div>
                     </div>
-                    <input
-                        type="range"
-                        className=" w-full  py-2 rounded-lg focus:outline-none focus:border-blue-400 "
-                        name="key"
-                    />
+                    <div>
+                        <p>キー</p>
+                        <input
+                            type="range"
+                            className=" w-full  py-2 rounded-lg focus:outline-none focus:border-blue-400 "
+                            name="key"
+                        />
+                    </div>
                     <textarea
                         name="memo"
                         className=" w-full px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400 "
