41e813dc6f4ebe057bc1406aa7d0eb714fbfed39
matsu <matsuaki171101@gmail.com>
2023-10-01 20:52:07 +0900

    ios用にh-screenを改良

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+const config = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            height: {
+                "screen": [
+                    "100vh", "100dvh"
+                ]
+            },
+            minHeight: {
+                "screen": [
+                    "100vh", "100dvh"
+                ]
+            },
+            maxHeight: {
+                "screen": [
+                    "100vh", "100dvh"
+                ]
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
