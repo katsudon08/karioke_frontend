f151b589e877ae1e93b80616d1b941a009b8f4e6
matsu <matsuaki171101@gmail.com>
2023-10-07 20:29:20 +0900

    ビルドエラーが直せない

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,17 +1,14 @@
-"use client"
-
 import { getLocalStrageSong } from "@/api";
 import DetailComponent from "@/components/DetailComponent";
 import { Song } from "@/types";
-import { useEffect } from "react";
 
 const page = () => {
-    const song: Song = getLocalStrageSong()
+    const songData: Song = getLocalStrageSong()
 
     return (
         <div className=" h-full flex flex-col justify-center items-center text-center break-all py-6 px-6 ">
             <main aria-label="曲情報詳細ページ" className=" bg-white h-fit w-full max-w-lg shadow-lg rounded-lg overflow-y-scroll hidden-scrollbar ">
-                <DetailComponent songData={song}/>
+                <DetailComponent songData={songData}/>
             </main>
         </div>
     );
