573fc7172cee91586ba41c61e56b743d69eeae6a
matsu <matsuaki171101@gmail.com>
2023-10-26 19:57:52 +0900

    やっぱりリロードできない

--- src/app/create/tag/page.tsx
+++ src/app/create/tag/page.tsx
@@ -20,7 +20,7 @@ const page = () => {
     return (
         <div className=" h-full flex flex-col justify-center items-center text-center break-all py-6 px-6 ">
             <main aria-label="曲情報作成ページ" className=" bg-white h-fit w-full max-w-lg shadow-lg rounded-lg overflow-y-scroll hidden-scrollbar ">
-                <TagComponent isCreate={true} checkedTags={checkedTags}/>
+                <TagComponent isCreate={true} checkedTags={checkedTags} tagIds={[]}/>
             </main>
         </div>
     );
